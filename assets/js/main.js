2968bfeeba167c4eb2745cc2e8fa75571514ca76
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-06-16 16:16:47 -0300

    chore: added box-shadow to header if certain y axis is reached out

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,13 @@ const linkAction = () => {
 };
 
 navLink.forEach((link) => link.addEventListener('click', linkAction));
+
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 50
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
