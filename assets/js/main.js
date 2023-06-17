7ec7b7a18f7e37104e02be24f354de1c57efa5a6
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-06-17 17:25:14 -0300

    chore: created scroll up button structure/styles & functionality that allow users to do it

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,13 @@ const scrollHeader = () => {
 };
 
 window.addEventListener('scroll', scrollHeader);
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+
+window.addEventListener('scroll', scrollUp);
